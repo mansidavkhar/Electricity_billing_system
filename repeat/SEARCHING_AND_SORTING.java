6d905576e3117a0d505aa49484a0a05df88b846c
Abhijay <148853650+abhijaydas09@users.noreply.github.com>
2024-04-02 20:25:48 +0530

    Create SEARCHING_AND_SORTING.java

---
+++ repeat/SEARCHING_AND_SORTING.java
@@ -0,0 +1,10 @@
+package user;
+
+import java.util.ArrayList;
+
+public interface SEARCHING_AND_SORTING {
+    // cannot run interface
+    void searcing_name(User user_name, ArrayList<User> user_list ) ;
+    void sorting_methods(ArrayList<User> user_list ,int left , int mid , int right) ;
+
+}
