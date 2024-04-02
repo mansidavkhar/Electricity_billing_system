c429e11f8612e760bc93458464a3d325f986c65b
Abhijay <148853650+abhijaydas09@users.noreply.github.com>
2024-04-02 20:36:19 +0530

    Add files via upload
    adding options to search and sort user name

---
+++ repeat/target/classes/user/Searching_name.java
@@ -0,0 +1,69 @@
+//package user;
+
+import java.util.ArrayList;
+//public class Searching_name implements SEARCHING_AND_SORTING {
+
+//    @Override
+//    public void searcing_name(User user_name, ArrayList<User> user_list) {
+//        int start = 0 ;
+//        int end = user_list.size();
+//        while (start <= end){
+//            int mid_index = start + (end - start)/2 ;
+//            User midElem = user_list.get(mid_index) ;
+//            if (midElem.equals(user_name)){
+//                //display the user details.
+//                for (User user : user_list) {
+//                    System.out.println(user);
+//                } // Assuming User class has a proper toString method
+//                break;
+////                String abc  ;
+////                abc.compareTo("abhijay ") ;
+//
+//            } else if (midElem.< 0) { // how to user compare method in string.
+//                start = mid_index + 1 ;
+//
+//            }
+//            else {
+//                end = mid_index - 1 ;
+//            }
+//            if (start > end){
+//                System.out.println("name" + user_name+  "not found in the array list ");
+//            }
+//
+//        }
+//
+//    }
+
+//    @Override
+//    public void sorting_methods(ArrayList<User> user_list , int left , int mid , int right ) {
+//        // size of two subarrays merge
+//        int n1 = mid - left + 1 ;
+//        int n2 = right - mid ;
+//
+//    }
+//}
+
+//binary search program
+//        int start = 0 ;
+//        int end = user_list.size();
+//        while (start <= end){
+//            int mid_index = start + (end - start)/2 ;
+//            String midElem = user_list.get(mid_index) ;
+//            if (midElem.equals(user_name)){
+//                //display the user details.
+//                for (String user : user_list) {
+//                    System.out.println(user);
+//                } // Assuming User class has a proper toString method
+//                break;
+//            } else if (midElem.compareTo(user_name) < 0) {
+//                start = mid_index + 1 ;
+//
+//            }else {
+//                end = mid_index - 1 ;
+//            }
+//            if (start > end){
+//                System.out.println("name" + user_name+  "not found in the array list ");
+//            }
+//
+//        } //
+//}
